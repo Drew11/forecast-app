5eefc09d26a72046f2ddd929a98e9732da8a95e5
Drew11 <drew.naumenko@gmail.com>
2019-07-14 18:41:08 +0300

    Updates

---
+++ precache-manifest.47bb9df9fa27d69eebb7a088b0aa6e1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c2346f2576118be10c10cc478505fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b47ab55e8f5af33d07ee",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d96fc1c1f85815637e35",
+    "url": "/static/css/main.66b577c8.chunk.css"
+  },
+  {
+    "revision": "b47ab55e8f5af33d07ee",
+    "url": "/static/js/2.1bdf3ee8.chunk.js"
+  },
+  {
+    "revision": "d96fc1c1f85815637e35",
+    "url": "/static/js/main.08317f2f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
