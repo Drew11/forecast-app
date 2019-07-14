e54a4e32d9d749d4cc3f4bde3b7986c5d9a8b34b
Drew11 <drew.naumenko@gmail.com>
2019-07-14 18:49:10 +0300

    Updates

---
+++ precache-manifest.317ae5280c6989e1984b51a771885c66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c662e3cf0bf71305df938cee8b969d87",
+    "url": "/forecast-app/index.html"
+  },
+  {
+    "revision": "b47ab55e8f5af33d07ee",
+    "url": "/forecast-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d96fc1c1f85815637e35",
+    "url": "/forecast-app/static/css/main.66b577c8.chunk.css"
+  },
+  {
+    "revision": "b47ab55e8f5af33d07ee",
+    "url": "/forecast-app/static/js/2.1bdf3ee8.chunk.js"
+  },
+  {
+    "revision": "d96fc1c1f85815637e35",
+    "url": "/forecast-app/static/js/main.08317f2f.chunk.js"
+  },
+  {
+    "revision": "1b8ff24b22f74ad27fb6",
+    "url": "/forecast-app/static/js/runtime~main.cfa6f48a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.47bb9df9fa27d69eebb7a088b0aa6e1f.js"
+  "/forecast-app/precache-manifest.317ae5280c6989e1984b51a771885c66.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/forecast-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
